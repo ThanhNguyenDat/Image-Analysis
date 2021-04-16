fbd62b18df3153e55f35b8ee21efe2ea47b5e38b
Nguyen Dat Thanh <76576719+ThanhNguyenDat@users.noreply.github.com>
2021-04-16 23:43:20 +0700

    Create Histogram_of_Grayscale_Image.py

---
+++ Histogram_of_Grayscale_Image.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+rcParams['figure.figsize'] = 8, 4
+
+path = ''
+img = cv2.imread(path)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+plt.hist(gray.ravel(), 256, [0, 256])
+plt.title("Histogram of Grayscale Image")
+plt.show()
