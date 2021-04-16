e1f4d6e866a20fddb6e01e6e9d03d543a832bc61
Nguyen Dat Thanh <76576719+ThanhNguyenDat@users.noreply.github.com>
2021-04-16 23:49:02 +0700

    Create Histogram_of_RGB_Image.py

---
+++ Histogram_of_RGB_Image.py
@@ -0,0 +1,13 @@
+import cv2
+
+rcParams['figure.figsize'] = 8, 4
+
+path = ''
+img = cv2.imread(path)
+
+color = ('b','g','r')
+for i,col in enumerate(color):
+    histr = cv2.calcHist([img],[i],None,[256],[0,256])
+    plt.plot(histr,color = col)
+    plt.xlim([0,256])
+plt.show()
